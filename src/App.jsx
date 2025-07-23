6720a16894af015b14bf32cbec43824789ed9308
Dev-Yadnesh8 <142681172+Dev-Yadnesh8@users.noreply.github.com>
2025-07-23 21:50:12 +0530

    basic project setup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div class="bg-primary text-white p-4 rounded-md ">
+        Primary background
+      </div>
+
+      <p class="text-gray-700">Dark gray text</p>
+
+      <button class="bg-green hover:bg-green/80 text-white px-4 py-2 rounded">
+        Confirm
+      </button>
+    </>
+  );
+}
+
+export default App;
